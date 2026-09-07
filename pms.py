17810cd0a6b3e932781258fe030e8a901c2d5077
Hunain Shaikh <shaikhhunain12@gmail.com>
2026-09-07 11:00:24 +0530

    v1.0 - basic medicine list

---
+++ pms.py
@@ -0,0 +1,9 @@
+# Pharmacy Management System - Version 1.0
+# Feature: display medicine list
+
+meds = ["Paracetamol", "Crocin", "Dolo"]
+
+def printmed():
+  for i in meds:
+    print(i)
+printmed()
